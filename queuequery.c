2467781a114674d6b5a0db60effecc65ab4e9281
Matthew McGovern (HE/HIM) <mamcgove@microsoft.com>
2021-12-07 13:09:15 -0800

    remove unneeded headers

--- queuequery.c
+++ queuequery.c
@@ -12,11 +12,7 @@
 #include <rte_memory.h>
 #include <rte_launch.h>
 #include <rte_eal.h>
-#include <rte_per_lcore.h>
-#include <rte_lcore.h>
 #include <rte_ethdev.h>
-#include <rte_udp.h>
-#include <rte_ip.h>
 
 
 int main(int argc, char** argv){
